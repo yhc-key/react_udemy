6c8ed800a35817d2d8e74f00cf747f8c368b6b63
yhc-key <yhcho0712@gmail.com>
2024-01-04 00:30:50 +0900

    NavLink and end

--- project21~30/section21_project/src/App.js
+++ project21~30/section21_project/src/App.js
@@ -1,5 +1,39 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />}/>
+//     <Route path="/products" element={<ProductsPage />}/>
+//   </Route>
+// ); //  jsx사용 방법
+
+// const router = createBrowserRouter(routerDefinitions);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+  {},
+]); // 오브젝트 사용방법
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ project21~30/section21_project/src/pages/Home.js
@@ -0,0 +1,23 @@
+import { Link, useNavigate } from "react-router-dom";
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  function navigatedHandler() {
+    navigate('/products')
+  }
+
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        go to <Link to="/products">the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigatedHandler}>Navigate</button>
+      </p>
+    </>
+  );
+}
+
+export default HomePage;
