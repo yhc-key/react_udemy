d6bf03baa16aa0461c5c4b13c46dc0a502186d21
yhc-key <yhcho0712@gmail.com>
2024-01-04 20:40:49 +0900

    practice cmd+t, extensions

--- project21~30/section21_project/src/pages/Error.js
+++ project21~30/section21_project/src/pages/Error.js
@@ -1,13 +1,15 @@
 import MainNavigation from "../components/MainNavigation";
 
 function ErrorPage() {
-    return <>
-    <MainNavigation/>
-    <main>
+  return (
+    <>
+      <MainNavigation />
+      <main>
         <h1>An error occurred</h1>
         <p>Could not find this page!</p>
-    </main>
+      </main>
     </>
+  );
 }
 
 export default ErrorPage;
