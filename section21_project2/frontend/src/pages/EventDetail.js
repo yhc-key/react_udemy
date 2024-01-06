a316b76a8dbc8c4d1370b7ec266b4f8692e49544
yhc-key <yhcho0712@gmail.com>
2024-01-06 18:56:38 +0900

    front router setting

---
+++ section21_project2/frontend/src/pages/EventDetail.js
@@ -0,0 +1,7 @@
+
+function EventDetailPage() {
+    return <h2>event</h2>
+}
+
+export default EventDetailPage
+
---
+++ section21_project2/frontend/src/pages/Home.js
@@ -0,0 +1,6 @@
+
+function HomePage() {
+    return <h1> Hi~</h1>
+}
+
+export default HomePage;
