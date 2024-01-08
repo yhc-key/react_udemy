dafd724a4b03855b3191f2b9941159a6b1ae72e9
yhc-key <yhcho0712@gmail.com>
2024-01-09 00:18:52 +0900

    submit add

--- section21_project2/frontend/src/components/EventForm.js
+++ section21_project2/frontend/src/components/EventForm.js
@@ -1,34 +1,63 @@
-import { Form } from 'react-router-dom';
+import { Form, useNavigation } from "react-router-dom";
 
-import classes from './EventForm.module.css';
+import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
-  function cancelHandler() {
-  }
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
+  function cancelHandler() {}
 
   return (
-    <Form method='post' className={classes.form}>
+    <Form method="post" className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required defaultValue={event? event.title :''} />
+        <input
+          id="title"
+          type="text"
+          name="title"
+          required
+          defaultValue={event ? event.title : ""}
+        />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required defaultValue={event? event.image :''} />
+        <input
+          id="image"
+          type="url"
+          name="image"
+          required
+          defaultValue={event ? event.image : ""}
+        />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required defaultValue={event? event.date :''} />
+        <input
+          id="date"
+          type="date"
+          name="date"
+          required
+          defaultValue={event ? event.date : ""}
+        />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required defaultValue={event? event.description :''} />
+        <textarea
+          id="description"
+          name="description"
+          rows="5"
+          required
+          defaultValue={event ? event.description : ""}
+        />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : 'Save'}
+        </button>
       </div>
     </Form>
   );
