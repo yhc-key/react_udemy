21e6e9a8e75362e68a6c3f66709add9951dff5cb
yhc-key <yhcho0712@gmail.com>
2024-01-14 12:42:59 +0900

    using useSearchParams

--- project21~30/section22_project/frontend/src/components/MainNavigation.js
+++ project21~30/section22_project/frontend/src/components/MainNavigation.js
@@ -39,6 +39,17 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
+          
+          <li>
+            <NavLink
+              to="/auth?mode=login"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Authentication
+            </NavLink>
+          </li>
         </ul>
       </nav>
       <NewsletterSignup />
